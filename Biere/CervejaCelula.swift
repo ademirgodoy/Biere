a0e5ae21752863ddd2fee6533ab027873b03f411
ademirgodoy <ademirgodoy@yahoo.com>
2020-12-07 19:53:53 -0400

    acerto dados

--- Biere/CervejaCelula.swift
+++ Biere/CervejaCelula.swift
@@ -2,12 +2,14 @@
 //  CervejaCelula.swift
 //  Biere
 //
-//  Created by ademir on 30/11/20.
+//  Created by ademir on 02/12/20.
 //
+
 import Foundation
 import UIKit
 
 class CervejaCelula: UITableViewCell {
-
-    
+   
+    @IBOutlet weak var cervLabel: UILabel!
+    @IBOutlet weak var detCervLabel: UILabel!
 }
